ed910e3f0dfd4df9dcdfcba0506d0de07ac3405b
D E L L <D E L L@DESKTOP-4P11DVP>
2026-07-02 01:00:43 +0530

    Add project files.

---
+++ MyAPITesting/Migrations/20260701172802_AddDepartment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAPITesting.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContactNumber",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactNumber",
+                table: "Employees");
+        }
+    }
+}
